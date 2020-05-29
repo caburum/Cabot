a854b8feb71a4980dcda8c902b60af300cfce051
Calum Dingwall <24cdingwall@gmail.com>
2020-05-29 14:33:45 +0000

    uptime api

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api', function(req, res) {
-  res.json({"guilds": client.guilds.cache.size, "channels": client.channels.cache.size, "users": client.users.cache.size});
+  res.json({"guilds": client.guilds.cache.size, "channels": client.channels.cache.size, "users": client.users.cache.size, "uptime": client.uptime});
 });
 
 app.get('/api/guilds', function(req, res) {
@@ -37,6 +37,9 @@ app.get('/api/users', function(req, res) {
   res.send(`${client.users.cache.size}`);
 });
 
+app.get('/api/uptime', function(req, res) {
+  res.send(`${client.uptime}`);
+});
 app.get('/api/testsend/:message', function(req, res) {
   client.channels.cache.get('699271873279557652').send(`${req.params.message}`);
   res.send(`sent '${req.params.message}'`)
