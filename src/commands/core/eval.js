35b35f9cceb088420c406d40d380744b701c68ea
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2021-09-29 10:57:58 -0400

    improve eval

--- src/commands/core/eval.js
+++ src/commands/core/eval.js
@@ -7,7 +7,7 @@ const life = 42;
 
 class EvalCommand extends Command {
 	description = 'Evaluates JavaScript code';
-	aliases = ['parse'];
+	aliases = ['parse', 'exec'];
 	slashCommand = false;
 
 	static get deps() {
@@ -17,49 +17,47 @@ class EvalCommand extends Command {
 	}
 
 	filter(message) {
-		console.log(this.config.owners.includes(message.author.id))
 		return this.config.owners.includes(message.author.id);
 	}
 
 	async call(message, content, extra) {
 		if (!content) return message.react(this.config.emoji.error);
-console.log(content)
+
 		try {
+			if (/^```js(?:on)?.*```$/is.test(content)) content = content.replace(/^```js(?:on)?|```$/gis, ''); // Extract from code block
+
 			const evaluated = eval(content);
 
-			if (content.toLowerCase().startsWith('message.channel.send(')) {
-				return;
-			}
+			if (content.toLowerCase().startsWith('message.channel.send(')) return;
 
-			let output = evaluated,
-				format = 'js';
-			if (typeof(evaluated) == 'object') {
-				output = Beautify(JSON.stringify(evaluated), {format: 'json'});
-				format = 'json';
-			}
-
-			let embed = new MessageEmbed()
-				.addField('**To Evaluate**', `\`\`\`js\n${Beautify(this.clean(content), {format: 'js'})}\`\`\``)
-				.addField('**Result**', '```' + format + '\n' + this.clean(output) + '```')
+			message.channel.send({ embeds: [ new MessageEmbed()
+				.addField('**To Evaluate**', this.clean(content))
+				.addField('**Result**', this.clean(evaluated, typeof(evaluated) === 'object'))
 				.addField('**Type**', typeof(evaluated))
 				.setTimestamp()
-				.setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
-			message.channel.send({embeds: [embed]});
+				.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+			] });
 		} catch (e) {
-			let errorembed = new MessageEmbed()
+			message.channel.send({ embeds: [ new MessageEmbed()
 				.setTitle(this.config.emoji.error + ' Something went wrong!')
 				.setDescription(e.toString())
 				.setTimestamp()
-				.setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
-			message.channel.send({embeds: [errorembed]});
+				.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+			] });
 		}
 	}
 
-	clean(input) { // Ensures the string is clean and will not cause issues
-		return new String(input)
-			.replace(process.env.token, 'Mjlx3zamasNOsd23fdz1sda23wO2Yw.TOKENz.emVjV4lcSzbdsjlz4U') // Fake token
-			.replace(/`/g, '`' + String.fromCharCode(8203))
-			.replace(/@/g, '@' + String.fromCharCode(8203));
+	clean(input, isJSON = false) { // Ensures the string is clean and will not cause issues
+		let format = isJSON ? 'json' : 'js';
+		if (isJSON) input = JSON.stringify(input);
+		return this.bot.format.codeBlock(
+			Beautify(
+				new String(input)
+					.replace(this.config.token, 'Mjlx3zamasNOsd23fdz1sda23wO2Yw.TOKENz.emVjV4lcSzbdsjlz4U') // Fake token
+					.replace(/`/g, '`' + String.fromCharCode(8203))
+					.replace(/@/g, '@' + String.fromCharCode(8203)),
+			{ format }),
+		format);
 	}
 };
 
